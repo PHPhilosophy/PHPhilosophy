b0f06c3f62133f5beeb5e40b9771fc4ff7a5e89b
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-13 08:50:59 +0200

    Add methods to the input interface

--- src/Http/Interfaces/InputInterface.php
+++ src/Http/Interfaces/InputInterface.php
@@ -11,4 +11,63 @@ namespace Phphilosophy\Http\Interfaces;
  */
 class InputInterface {
     
+    /**
+     * @param   array   $get
+     *
+     * @return  self
+     */
+    public function withGet(array $get);
+    
+    /**
+     * @param   array   $post
+     *
+     * @return  self
+     */
+    public function withPost(array $post);
+    
+    /**
+     * @param   string  $key
+     * @param   mixed   $value
+     *
+     * @return  self
+     */
+    public function withAddedGet(string $key, $value);
+    
+    /**
+     * @param   string  $key
+     * @param   mixed   $value
+     *
+     * @return  self
+     */
+    public function withAddedPost(string $key, $value);
+    
+    /**
+     * @param   string  $key
+     *
+     * @return  self
+     */
+    public function withoutGet(string $key);
+    
+    /**
+     * @param   string  $key
+     *
+     * @return  self
+     */
+    public function withoutPost(string $key);
+    
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function get($name = null, $default = null);
+    
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function post($name = null, $default = null);
 }
