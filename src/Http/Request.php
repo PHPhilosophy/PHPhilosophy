68fd8a724d9ad5d1f2a7b684101b9586161a56ac
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-13 06:46:59 +0200

    Wire up the request

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -3,6 +3,7 @@
 namespace Phphilosophy\Http;
 
 use Phphilosophy\Http\Interfaces\RequestInterface;
+use Phphilosophy\Http\Interfaces\{InputInterface, SessionInterface, UriInterface};
 
 /**
  * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
@@ -13,4 +14,41 @@ use Phphilosophy\Http\Interfaces\RequestInterface;
  */
 class Request implements RequestInterface {
     
+    /**
+     * @var string
+     */
+    private $method;
+    
+    /**
+     * @var \Phphilosophy\Http\Interfaces\UriInterface
+     */
+    private $uri;
+    
+    /**
+     * @var \Phphilosophy\Http\Interfaces\InputInterface
+     */
+    private $input;
+    
+    /**
+     * @var \Phphilosophy\Http\Interfaces\SessionInterface
+     */
+    private $session;
+    
+    /**
+     * @param   string                                          $method
+     * @param   \Phphilosophy\Http\Interfaces\UriInterface      $uri
+     * @param   \Phphilosophy\Http\Interfaces\InputInterface    $input
+     * @param   \Phphilosophy\Http\Interfaces\SessionInterface  $session
+     */
+    public function __construct(
+        $method,
+        UriInterface $uri,
+        InputInterface $input,
+        SessionInterface $session
+    ) {
+        $this->method = $method;
+        $this->uri = $uri;
+        $this->input = $input;
+        $this->session = $session;
+    }
 }
