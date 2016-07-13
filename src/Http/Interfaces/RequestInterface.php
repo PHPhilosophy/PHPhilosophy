bc26e5d2929b90903e74c657f74c41af47b2845e
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-13 09:04:52 +0200

    Add methods to the request interface

--- src/Http/Interfaces/RequestInterface.php
+++ src/Http/Interfaces/RequestInterface.php
@@ -11,4 +11,73 @@ namespace Phphilosophy\Http\Interfaces;
  */
 class RequestInterface {
     
+    /**
+     * @return  string
+     */
+    public function getMethod();
+    
+    /**
+     * @return  \Phphilosophy\Http\Interfaces\UriInterface
+     */
+    public function getUri();
+    
+    /**
+     * @return  \Phphilosophy\Http\Interfaces\InputInterface
+     */
+    public function getInput();
+    
+    /**
+     * @return  \Phphilosophy\Http\Interfaces\SessionInterface
+     */
+    public function getSession();
+    
+    /**
+     * @param   string  $method
+     *
+     * @return  self
+     */
+    public function withMethod(string $method);
+    
+    /**
+     * @param   \Phphilosophy\Http\Interfaces\UriInterface  $uri
+     *
+     * @return  self
+     */
+    public function withUri(UriInterface $uri);
+    
+    /**
+     * @param   \Phphilosophy\Http\Interfaces\InputInterface    $input
+     *
+     * @return  self
+     */
+    public function withInput(InputInterface $input);
+    
+    /**
+     * @param   \Phphilosophy\Http\Interfaces\SessionInterface  $session
+     */
+    public function withSession(SessionInterface $session);
+    
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function get($name = null, $default = null);
+    
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function post($name = null, $default = null);
+    
+    /**
+     * @param   string  $name
+     * @param   mixed   $default
+     *
+     * @return  mixed
+     */
+    public function session($name = null, $default = null);
 }
