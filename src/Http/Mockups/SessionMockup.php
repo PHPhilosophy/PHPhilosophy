6135af2130f7ff6ecb8aa418437de50dd8c923dc
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-13 07:06:52 +0200

    Add SessionMockup for Unit Testing

---
+++ src/Http/Mockups/SessionMockup.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Phphilosophy\Http\Mockups;
+
+use Phphilosophy\Http\Interfaces\SessionInterface;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016 Lisa Saalfrank
+ * @license     MIT License http://opensource.org/licenses/MIT
+ * @package     Phphilosophy\Http
+ * @version     1.0-thales
+ */
+class SessionMockup implements SessionInterface {
+    
+}
