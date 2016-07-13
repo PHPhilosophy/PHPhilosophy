95e2730041a87eb7f3a036f416378a003a534da7
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-13 05:45:23 +0200

    Add input, response, session and uri file

---
+++ src/Http/Session.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Phphilosophy\Http;
+
+/**
+ * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
+ * @copyright   2016 Lisa Saalfrank
+ * @license     MIT License http://opensource.org/licenses/MIT
+ * @package     Phphilosophy\Http
+ * @version     1.0-thales
+ */
+class Session {
+    
+}
