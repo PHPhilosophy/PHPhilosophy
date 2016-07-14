687920bcf486c689c10aefeec716ef31d87716a8
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-14 04:23:22 +0200

    Add missing uri methods

--- src/Http/Uri.php
+++ src/Http/Uri.php
@@ -19,16 +19,53 @@ class Uri implements UriInterface {
     private $uri;
     
     /**
+     * @var array
+     */
+    private $segments = [];
+    
+    /**
      * @param   string  $uri
      */
-    public function __construct($uri) {
+    public function __construct(string $uri)
+    {
         $this->uri = $uri;
+        $this->findSegments();
     }
     
     /**
      * @return  string
      */
-    public function getUri() {
+    public function getUri():string {
         return $this->uri;
+    }
+    
+    /**
+     * @return  void
+     */
+    public function findSegments()
+    {
+        // Normalize the uri and pattern
+        $uri = trim($this->uri, '/');
+        
+        // Split it into its segments
+        $segments = explode('/', $uri);
+        
+        $this->segments = $segments;
+    }
+    
+    /**
+     * @return  array
+     */
+    public function getSegments():array {
+        return $this->segments;
+    }
+    
+    /**
+     * @param   int     $key
+     *
+     * @return  string
+     */
+    public function getSegment(int $key):string {
+        return $this->segments[$key];
     }
 }
