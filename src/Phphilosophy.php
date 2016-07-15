000d63822a9580157aa25af61ffb90f3cc451c71
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-07-15 09:00:08 +0200

    Add creation of routes to the base class

--- src/Phphilosophy.php
+++ src/Phphilosophy.php
@@ -3,6 +3,7 @@
 namespace Phphilosophy;
 
 use Phphilosophy\Http\Interfaces\RequestInterface;
+use Phphilosophy\Router\Route;
 
 /**
  * @author      Lisa Saalfrank <lisa.saalfrank@web.de>
@@ -33,7 +34,7 @@ class Phphilosophy {
      * @return  void
      */
     public function get(string $pattern, array $action, array $middleware = []) {
-        
+        $this->add(['GET'], $pattern, $action, $middleware);
     }
     
     /**
@@ -44,7 +45,7 @@ class Phphilosophy {
      * @return  void
      */
     public function post(string $pattern, array $action, array $middleware = []) {
-        
+        $this->add(['POST'], $pattern, $action, $middleware);
     }
     
     /**
@@ -55,7 +56,7 @@ class Phphilosophy {
      * @return  void
      */
     public function any(string $pattern, array $action, array $middleware = []) {
-        
+        $this->add(['GET', 'POST'], $pattern, $action, $middleware);
     }
     
     /**
@@ -67,7 +68,7 @@ class Phphilosophy {
      * @return  void
      */
     public function add(array $methods, string $pattern, array $action, array $middleware = []) {
-        
+        $route = new Route($pattern, $actions, $methods, $middleware);
     }
     
     /**
