6287bef07268475b9698f236b75ce7250a6d7162
Lisa Saalfrank <lsaalfrank@hof-university.de>
2016-08-01 21:27:33 +0200

    Fix Bug in SessionInterface

--- src/Http/Interfaces/SessionInterface.php
+++ src/Http/Interfaces/SessionInterface.php
@@ -9,7 +9,7 @@ namespace Phphilosophy\Http\Interfaces;
  * @package     Phphilosophy\Http\Interfaces
  * @version     1.0-thales
  */
-class SessionInterface {
+interface SessionInterface {
     
     /**
      * @param   string  $name
